2eb4bb5c2b830d53d2f8ee19958848f5dc11affe
Bek-star <214883516+BEK-star@users.noreply.github.com>
2025-08-12 21:07:28 +0600

    Understanding Loop

---
+++ basics/Loop.go
@@ -0,0 +1,27 @@
+package basics
+
+import "fmt"
+
+func main() {
+	//Simple iteration of the range
+	//for i := 1; i <= 5; i++ {
+	//fmt.Println(i)
+	//}
+
+	// Iterate over collection
+	//numbers := []int{1, 2, 3, 4, 5, 6}
+	//for index, value := range numbers {
+	//	fmt.Printf("Index %d, Value %d\n", index, value)
+	//}
+
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
+		if i == 5 {
+			break
+		}
+
+	}
+}
