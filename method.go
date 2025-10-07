c6b39352fec67d4ddbe1ed3af8f39bed4c862c30
Bek-star <214883516+BEK-star@users.noreply.github.com>
2025-10-07 19:49:01 +0600

    method

---
+++ method.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Rectangle struct {
+	width  float64
+	length float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.length * r.width
+}
+
+func (r *Rectangle) Scale(factor float64) {
+	r.length = r.length * factor
+	r.width = r.width * factor
+}
+
+func main() {
+
+	rect := Rectangle{
+		width:  9,
+		length: 5,
+	}
+
+	ar := rect.Area()
+	fmt.Println("Area before scaling:", ar)
+
+	rect.Scale(2)
+	ar = rect.Area()
+
+	fmt.Println("Area after scaling:", ar)
+
+}
